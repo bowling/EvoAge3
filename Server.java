207eca9bc6f0ecb04a182555f1ea41daf228f705
Michael Redbourne <mredbourne12@gmail.com>
2019-06-12 08:29:46 -0300

    Update Server.java
    Added in JDBC (Non functional). Server username and password removed.
    
    Client updated to talk to server over Internet. Implementation needed for JSSE

--- Server.java
+++ Server.java
@@ -1,12 +1,16 @@
 import java.io.*;
 import java.net.*;
+import java.util.Date;
+import java.sql.*; 
+import java.util.Scanner;
 
 /* Server login system. I have not updated the program to connect to mySQL yet. */
 
 public class Server extends Thread {
 
     public static final int PORT = 3332;
-    public static final int BUFFER_SIZE = 626;
+    public static final int BUFFER_SIZE = 10;
+	Scanner scan = null;
 
 	//Creating server
     public void run() {
@@ -22,10 +26,14 @@ public class Server extends Thread {
     }
 
     private void saveAuthenticationAttempt(Socket socket) throws Exception {
+		Date date = new Date();
+		long time = date.getTime();
+		Timestamp ts = new Timestamp(time);
+		System.out.println("Authentication Start: " + ts);
         InputStream inputStream = socket.getInputStream();
         FileOutputStream fOutputStream = new FileOutputStream("C:\\Users\\Michael\\Desktop\\Evony\\authenticationReceived.sql");
 
-        byte[] serverBArray = new byte[1024]; // fix the hard coded value here.
+        byte[] serverBArray = new byte[1]; // fix the hard coded value here.
         System.out.println("Reading file from server...");
         BufferedOutputStream bOutputStream = new BufferedOutputStream(fOutputStream);
         int bytesRead;
@@ -35,9 +43,53 @@ public class Server extends Thread {
         bOutputStream.close();
         System.out.println("Writing file complete...");
 		
+		
 		/* Connect to mySQL DB and send file (prepared statement inside SQL file) */
 		/* Do NOT close to Socket! */
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+		}
+		catch(ClassNotFoundException e){
+			System.out.println("Class not found...");
+			return;
+		}
+		try{
+			Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/evonydb","GIVE USERNAME","GIVE PASSWORD").newInstance();
+			System.out.println("Here.");
+			Statement stmt = conn.createStatement();
+			File f = new File("authenticationReceived.sql");
+			
+			String query = scan.nextLine();
+			System.out.println(query);
+			ResultSet rs = stmt.executeQuery(query);
+			
+			if(rs.getInt(1) == 1){
+				System.out.println("Authentication Success");
+				
+				//Send update reply to client
+				
+				conn.close();				
+			}
+			else{
+				System.out.println("Authentication Failed. Retry username and password.");
+				conn.close();
+				return;
+			}
+		}
+		catch(SQLException sqlExce){
+			System.out.println("SQL Exception Occured."); // Write to logfile.
+			return;
+		}
 		
+		
+		/* Time Completed */
+
+		inputStream.close();
+		
+		Date date2 = new Date();
+		long time2 = date.getTime();
+		Timestamp ts2 = new Timestamp(time2);
+		System.out.println("Authentication Complete: " + ts2);
     }
 
     public static void main(String[] args) {
